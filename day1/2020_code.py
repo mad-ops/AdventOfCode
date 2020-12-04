f928f792a026079ef25ee8ddae80f862f63f02ba
Vish <vish@mad-ops.com>
2020-12-04 11:04:56 -0500

    Day 1 complete

--- day1/2020_code.py
+++ day1/2020_code.py
@@ -5,6 +5,8 @@ with open(os.path.join(scriptdir,'2020_input.txt')) as expenses:
     expenseList = [int(x.strip()) for x in expenses.readlines()]
 
 #part one
+## very simple, create a cache of numbers we're
+## searching for and return when we find one
 def pairwise_haystack(expenses):
     cache = []
     for expense in expenses:
@@ -15,8 +17,21 @@ def pairwise_haystack(expenses):
 
 #part two
 import itertools
+from math import prod
 def trip_haystack(expenses):
-    return [sum(i) for i in list(itertools.combinations(expenses, 2))]
+    # Lets create a dict key'd by sum of pairs
+    pairwise_expenses = {}
+    for pair in list(itertools.combinations(expenses, 2)):
+        if sum(pair) > 2020:
+            continue
+
+        pairwise_expenses[sum(pair)] = pair
+
+    # Loop through expenses to see if we have
+    # the matching pairwise sum, then return
+    for expense in expenses:
+        if 2020 - expense in pairwise_expenses:
+            return expense * prod(pairwise_expenses[2020-expense])
 
 if __name__ == "__main__":
     print(pairwise_haystack(expenseList))
